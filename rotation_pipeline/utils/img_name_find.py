1e54756d2d568e47eccfa29ef92a1ac6815d18c8
cyanCity <92922931+Cyancity27@users.noreply.github.com>
2025-08-21 18:22:55 +0800

    Delete rotation_pipeline/utils/img_name_find.py

--- rotation_pipeline/utils/img_name_find.py
+++
@@ -1,8 +0,0 @@
-import os
-
-root_dir = '/gemini/platform/public/aigc/cv_banc/zhanghy56_intern/cjr/blender_cjr/blender/outputs/sculpture4/output_outdoor7_sculpture4/test_masks_cryptomatte2'  # 请替换为你的目标路径
-
-for current_dir, dirs, files in os.walk(root_dir):
-    for file in files:
-        if file.lower().endswith('.png'):
-            print(file)
